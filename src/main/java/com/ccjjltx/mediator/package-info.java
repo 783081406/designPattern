8f61d321599eb1d9ebdaf681628a0bfd24bdaf11
783081406 <783081406ccj@gmail.com>
2018-05-29 23:31:26 +0800

    中介者模式

---
+++ src/main/java/com/ccjjltx/mediator/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Mediator模式也叫中介者模式
+ * Mediator模式是行为模式之一
+ * 在Mediator模式中，类之间的交互模式被同一放在Mediator的对象中，对象通过Mediator对象同其他对象交互
+ * Mediator对象起着控制器的作用
+ *
+ * @author ccj
+ * @version 1.0
+ * @since 1.8
+ */
+package com.ccjjltx.mediator;
