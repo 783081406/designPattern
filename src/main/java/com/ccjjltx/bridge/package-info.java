8735bed85a1e90440ae184f94442640ad1d933d8
783081406 <783081406ccj@gmail.com>
2018-05-29 22:57:22 +0800

    桥接模式概述

---
+++ src/main/java/com/ccjjltx/bridge/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Bridge模式有叫做桥接模式，是构造型的设计模式之一
+ * Bridge模式基于类的最小设计原则，通过使用封装，聚合以及继承等行为来让不同的类承担不同的责任
+ * 他的主要特点是抽象(abstraction)与行为实现(implementation)分离开来，
+ * 从而可以保持各部分的独立性以及应对它们的功能拓展
+ *
+ * @author ccj
+ * @version 1.0
+ * @since 1.8
+ */
+package com.ccjjltx.bridge;
