a9b5dfb00885224d4c76228f04d68c8afb92fc5f
783081406 <783081406ccj@gmail.com>
2018-05-29 23:18:07 +0800

    原型模式

---
+++ src/main/java/com/ccjjltx/prototype/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 原型模式：
+ * Prototype模型是一种对象创建型模式，它采取复制原型对象的方法来创建对象的实例。
+ * 使用Prototype模式创建的实例，具有与原型一样的数据
+ *
+ * @author ccj
+ * @version 1.0
+ * @since 1.8
+ */
+package com.ccjjltx.prototype;
