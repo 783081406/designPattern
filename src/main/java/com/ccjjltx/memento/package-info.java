c6a2e3d1dcfc6bc9a5e4fab726e2d6789193242b
783081406 <783081406ccj@gmail.com>
2018-05-29 16:57:13 +0800

    备忘录模式

---
+++ src/main/java/com/ccjjltx/memento/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 备忘录模式：
+ * memento模式也叫做备忘录模式，是行为模式之一，他的作用是保存对象的内部状态，
+ * 并需要的时候(undo/rollback)恢复对象以前的状态
+ *
+ * @author ccj
+ * @version 1.0
+ * @since 1.8
+ */
+package com.ccjjltx.memento;
